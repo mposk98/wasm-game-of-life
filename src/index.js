9e88d867f57bef358d395906d3e56ad745ce6f02
mposk98 <anythingand42@gmail.com>
2020-01-19 04:34:17 +0300

    add some settings

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import { Universe, Cell, UniverseMode } from 'wasm-game-of-life-rust'; // eslint
 import { memory } from 'wasm-game-of-life-rust/wasm_game_of_life_bg'; // eslint-disable-line import/no-unresolved
 import scene from './scene';
 
-const ROWS = 100;
-const COLUMNS = 100;
-const universe = Universe.new(
+const ROWS = 20;
+const COLUMNS = 20;
+let universe = Universe.new(
     ROWS,
     COLUMNS,
     UniverseMode.FixedSizeNonPeriodic,
 );
 document.addEventListener('keydown', (event) => {
     if (event.keyCode === 32) {
+        event.preventDefault();
         universe.tick();
     }
 });
@@ -22,6 +23,13 @@ const RIGHT_BUTTON = 3;
 scene.addClickListener((row, col) => universe.toggle_cell(row, col));
 scene.addMousePressedListener(LEFT_BUTTON, (row, col) => universe.set_alive(row, col));
 scene.addMousePressedListener(RIGHT_BUTTON, (row, col) => universe.set_dead(row, col));
+
+document.getElementById('universe-size').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const size = event.target.elements.size.value;
+    universe = Universe.new(size, size, UniverseMode.FixedSizeNonPeriodic);
+    scene.reinit(size, size);
+});
 
 const renderLoop = () => {
     const cellsPtr = universe.cells();
